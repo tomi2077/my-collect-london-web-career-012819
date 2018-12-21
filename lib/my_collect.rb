a672b419b17282e28a2a909c89ccff0c4154ae90
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-21 10:17:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,15 +3,8 @@ def my_collect(array)
   i = 0
   while i < array.length
   yield(my_yield[i])
+  i += 1
   end
   my_yield
 end
   
-
-def hamburger(toppings)
-  my_statements = []
-  toppings.each do |topping|
-    my_statements << "I love #{topping} on my burgers!"
-  end
-  my_statements
-end
