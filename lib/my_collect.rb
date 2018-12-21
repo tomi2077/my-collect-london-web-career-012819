bede0af263f8d97f228f32fd2546c07bc2331793
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-21 10:22:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,20 +2,10 @@ def my_collect(array)
   my_yield = []
   i = 0
   while i < array.length
-  yield(my_yield << i)
+  yield(my_yield << |i| i.upcase)
   i += 1
   end
   my_yield
 end
-  
-def hamburger(toppings)
-  toppings.collect do |topping|
-    puts "I love #{topping} on my burgers!"
-  end
-end
+ 
 
-def burger(toppings)
-  toppings.collect do |topping|
-    "I love #{topping} on my burgers"
-  end
-end
